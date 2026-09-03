d2ce82d46f532a07868e01d9dd0e52824a4b7da0
Anton Lebedev <lebedev.ay@outlook.com>
2026-09-03 21:45:43 +0300

    dbt: убраны модель patch_impact, её тесты и источник dim_window

--- dbt/tests/patch_impact_unique_patch_sk_window_code.sql
+++
@@ -1,6 +0,0 @@
--- дублирует dbt_utils.unique_combination_of_columns: пакет
--- не подключён. Возвращает строки, где комбинация повторяется
-select patch_sk, window_code, count(*) as n
-from {{ ref('patch_impact') }}
-group by patch_sk, window_code
-having count(*) > 1
