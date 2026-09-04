5ce26d5b276e7d61db2e5304697959c9f2ba354e
Anton Lebedev <lebedev.ay@outlook.com>
2026-09-04 22:41:05 +0300

    review_flat не тащит в витрину заглушку Unknown

--- dbt/models/review_flat.sql
+++ dbt/models/review_flat.sql
@@ -9,3 +9,6 @@ select g.app_id,
 from {{ source('core', 'fct_review') }} r
 join {{ source('core', 'dim_game') }} g on g.game_sk = r.game_sk
 join {{ source('core', 'dim_language') }} l on l.language_sk = r.language_sk
+-- заглушка Unknown не игра: без фильтра отзывы без распознанной версии
+-- уезжают в витрину как app_id = -1 и агрегируются отдельной «игрой»
+where g.app_id > 0
